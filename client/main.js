12be2a412f94e014c8d7c2ceb28e2c13dddeb329
Shreyans Gandhi <spgandhi@live.com>
2016-09-11 06:04:12 +0530

    6 am push

--- client/main.js
+++ client/main.js
@@ -1,48 +1,140 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
  
-angular.module('socially', [
+import WardrobeTemplate from '/client/wardrobe.html';
+import WardrobeAddTemplate from '/client/wardrobe-add.html';
+import RatingAddTemplate from '/client/rating-add.html';
+import HomeTemplate from '/client/home.html';
+
+var app = angular.module('socially', [
     angularMeteor,
-    'accounts.ui'
-  ])
-  .controller('PartiesListCtrl', ['$scope', function($scope) {
-  	
-  	$scope.answer = '';
-  	$scope.newItem = {};
-  	$scope.currentRatingItem = 0;
-  	$scope.addItem = function(){
-  		$scope.newItem.user = Meteor.user()._id;
-  		Wardrobe.insert($scope.newItem);
-  	}
+    'accounts.ui',
+    'ui.router'
+  ]);
 
-  	$scope.fetchdata = function(){
-  		$scope.items = Wardrobe.find().fetch();
-  		console.log($scope.items);
-  		$scope.ratings = Ratings.find().fetch(); //Condition
-  		$scope.rating = $scope.ratings[$scope.currentRatingItem];
-  	}
+app.config(function($stateProvider) {
+  
+  var home = {
+    name: 'home',
+    url: '/home',
+    templateUrl: HomeTemplate,
+    controller: 'Home'
+  }
 
-  	$scope.addRating = function(){
-  		$scope.newRating.result = {
-  			item1: 0,
-  			item2: 0
-  		}
-  		Ratings.insert($scope.newRating);
-  	}
+  var wardrobeAdd = {
+    name: 'wardrobe-add',
+    url: '/wardrobe/add',
+    templateUrl: WardrobeAddTemplate,
+    controller: 'WardrobeAdd'
+  }
 
-  	$scope.submitRating = function(){
-  		if($scope.answer=='1'){
-  			$scope.rating.result.item1++;
-  		}else if($scope.answer=='2'){
-  			$scope.rating.result.item2++;
-  		}
-  		$scope.answer = '';
-  		//update ratings.voted_by with user id
-  		Ratings.update({_id: $scope.rating._id}, $scope.rating);
-  		$scope.currentRatingItem++;
-  		if($scope.currentRatingItem < $scope.ratings.length)
-  			$scope.rating = $scope.ratings[$scope.currentRatingItem];
-  	}
+  var wardrobe = {
+    name: 'Wardrobe',
+    url: '/wardrobe/view/:id',
+    templateUrl: WardrobeTemplate,
+    controller: 'Wardrobe'
+  }
+
+  var ratingAdd = {
+    name: 'RatingAdd',
+    url: '/rating/add',
+    templateUrl: RatingAddTemplate,
+    controller: 'RatingAdd'
+  }
+
+  $stateProvider.state(home);
+  $stateProvider.state(wardrobe);
+  $stateProvider.state(wardrobeAdd);
+  $stateProvider.state(ratingAdd);
+
+  
+});
+
+app.controller('Wardrobe', ['$scope', '$stateParams', function ($scope, $stateParams) {
+  requested_user = Meteor.users.find({_id: $stateParams.id}).fetch();
+  if(requested_user.length == 0){
+    $scope.message = 'User does not exist';
+    return;
+  }
+  
+  if($stateParams.id == Meteor.user()._id){
+    $scope.items = Wardrobe.find({user: $stateParams.id}).fetch();
+    if($scope.items.length == 0)
+      $scope.message = "No wardrobe items";
+  }else{
+    if(Meteor.user().profile && Meteor.user().profile.sharedBy.indexOf($stateParams.id) > -1){
+      $scope.items = Wardrobe.find({user: $stateParams.id}).fetch();
+    }else{
+      $scope.message = 'Not Allowed';
+    }
+  }
+  console.log($scope.items);
+
+}])
+
+app.controller('WardrobeAdd', ['$scope', function ($scope) {
+  console.log('in add');
+  $scope.newItem = {};
+  $scope.addItem = function(){
+      $scope.newItem.user = Meteor.user()._id;
+      Wardrobe.insert($scope.newItem);
+      $scope.newItem = {};
+    }
+}])
+
+app.controller('RatingAdd', ['$scope', function ($scope) {
+  console.log('in raitng');
+  $scope.addRating = function(){
+    $scope.newRating.result = {
+      item1: 0,
+      item2: 0,
+      voted_by : [] //end time
+    }
+    Ratings.insert($scope.newRating);
+    $scope.newRating = {};
+  }
+}])
+
+app.controller('Home', ['$scope', function($scope) {
+    console.log('in home');
+    $scope.answer = '';
+    $scope.noMorePosts=false;
+
+    $scope.currentRatingItem = 0;
+
+    $scope.fetchdata = function(){
+      $scope.ratings = Ratings.find({voted_by: { "$ne" : Meteor.user()._id} }).fetch(); //Condition
+      if($scope.ratings.length == 0){
+        $scope.noMorePosts=true;
+        return;
+      }
+
+      console.log($scope.ratings);
+      $scope.rating = $scope.ratings[$scope.currentRatingItem];
+
+    }
+
+    $scope.submitRating = function(){
+      if($scope.answer=='1'){
+        $scope.rating.result.item1++;
+      }else if($scope.answer=='2'){
+        $scope.rating.result.item2++;
+      }
+      $scope.answer = '';
+      $scope.rating.result.voted_by.push(Meteor.user()._id);
+      //update ratings.voted_by with user
+      Ratings.update({_id: $scope.rating._id}, $scope.rating);
+      $scope.currentRatingItem++;
+      if($scope.currentRatingItem < $scope.ratings.length)
+        $scope.rating = $scope.ratings[$scope.currentRatingItem];
+      else
+         $scope.noMorePosts=true;
+    }
+
+    $scope.shareWardrobe = function(){
+      console.log($scope.shareWardrobeWith);
+      Meteor.call('shareWardrobe', $scope.shareWardrobeWith);
+    }
 
    
   }]);
--- server/main.js
+++ server/main.js
@@ -3,3 +3,41 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.methods({
+	shareWardrobe: function(email){
+		user = Meteor.users.find({'emails.address': email}).fetch();
+		console.log(user.length);
+		if(user.length > 0){
+			user = user[0];
+			if(user.profile && user.profile.sharedBy){
+				user.profile.sharedBy.push(Meteor.user()._id);
+			}else if(user.profile && !user.profile.sharedBy){
+				user.profile.sharedBy = [Meteor.user()._id]
+			}else{
+				user.profile = {
+					sharedBy: [Meteor.user()._id]
+				}
+			}
+		}else{
+			return;
+		}
+
+		other_id = user._id;
+		self_user = Meteor.user();
+
+		if(self_user.profile && self_user.profile.sharedWith){
+			self_user.profile.sharedWith.push(other_id);
+		}else if(self_user.profile && !self_user.profile.sharedWith){
+			self_user.profile.sharedWith = [other_id]
+		}else{
+			self_user.profile = {
+				sharedWith: [other_id]
+			}
+		}
+
+
+		Meteor.users.update({_id: user._id}, user);
+		Meteor.users.update({_id: Meteor.user()._id}, self_user);
+	}
+})
