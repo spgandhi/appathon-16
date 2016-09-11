80ffe42e20880ba7dc87412ecbff37908e9cf3cc
Shreyans Gandhi <spgandhi@live.com>
2016-09-11 13:39:59 +0530

    final one

--- mobile-config.js
+++ mobile-config.js
@@ -1,5 +1,26 @@
 App.info({
-  id: 'com.meteor.examples.todos',
-  name: 'Appathon-16',
-  version: "0.0.1"
+  id: 'com.dress-up.spgandhi.sir',
+  name: 'DressUP',
+  description: 'Go Fashionesta',
+  author: 'Shreyans Gandhi',
+  email: 'spgandhi@live.com',
+  website: 'travel.igandhi.com'
+});
+// Set up resources such as icons and launch screens.
+App.icons({
+  android_mdpi : 'icons/mipmap-mdpi/ic_launcher.png',
+  android_hdpi : 'icons/mipmap-hdpi/ic_launcher.png',
+  android_xhdpi : 'icons/mipmap-xhdpi/ic_launcher.png',
+  android_xxhdpi : 'icons/mipmap-xxhdpi/ic_launcher.png',
+  android_xxxhdpi : 'icons/mipmap-xxxhdpi/ic_launcher.png'
+});
+App.launchScreens({
+  android_mdpi_portrait : 'splash/mdpi/background.png',
+  android_mdpi_landscape : 'splash/mdpi/background.png',
+  android_hdpi_portrait : 'splash/hdpi/background.png',
+  android_hdpi_landscape : 'splash/hdpi/background.png',
+  android_xhdpi_portrait : 'splash/xhdpi/background.png',
+  android_xhdpi_landscape : 'splash/xhdpi/background.png',
+  android_xxhdpi_portrait : 'splash/xxhdpi/background.png',
+  android_xxhdpi_landscape : 'splash/xxhdpi/background.png'
 });
